fc1fa6e2ea9d0bc2d324b70bb5e3f55a4b68490b
Ed <peng@dcs-bbk-ac-uk>
2012-11-04 11:37:01 +0000

    rota-wk2

--- openwsn/02b-MAChigh/noiseprobe.c
+++ openwsn/02b-MAChigh/noiseprobe.c
@@ -123,11 +123,11 @@ inline void record(){
   }
   else{
     switch(idmanager_getMyID(ADDR_16B)->addr_16b[1]){
-      case DEBUG_MOTEID_3:
+      case DEBUG_MOTEID_4: //8
         /* static kalman filter */
         nf_vars.rssi[nf_vars.current] = kalman(raw,nf_vars.rssi[nf_vars.current],nf_vars.current); 
         break;
-      case DEBUG_MOTEID_4:  
+      case DEBUG_MOTEID_5: //9
         /* adapt Q for Kalman Filter */
         if(nf_vars.hourMark==HOURLYRUN){
             adjustQall();
@@ -137,10 +137,11 @@ inline void record(){
         }
         nf_vars.rssi[nf_vars.current] = adaptiveKalman(raw,nf_vars.rssi[nf_vars.current],nf_vars.current); 
         break;
-      case DEBUG_MOTEID_2:  
+      case DEBUG_MOTEID_3: //7
         /* Simple Exponential Smoothing */
         nf_vars.rssi[nf_vars.current] = brown_ses(0.1, raw, nf_vars.rssi[nf_vars.current], nf_vars.current);
-        break;    
+        break; 
+      case DEBUG_MOTEID_2: //6   
       default:
         nf_vars.rssi[nf_vars.current] = raw;
         break;
